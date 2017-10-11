bf47f2e671a7676eb49cd3dda2a097fbcb71b655
mer <imt.killian56@outlook.fr>
2017-10-11 16:51:21 +0200

    Ajout Fabriques; Ajout JavaDoc; Redaction ReadMe.

--- UV-Langages-de-programmation/src/tp1/Element.java
+++ UV-Langages-de-programmation/src/tp1/Element.java
@@ -1,27 +1,43 @@
 package tp1;
-
+/**
+ * An element of the the lane linked to another
+ * @param <E> the value of the element
+ */
 public class Element<E> {
-
+    /**Attribute to know the next element of the lane*/
     private Element next;
     private E value;
-
+    /**
+     * Constructor of an Element
+     * @param value - The value of the element
+     */
     public Element(E value) {
         this.next = null;
         this.value = value;
     }
-
+    /**
+     * @return the next element of the lane
+     */
     public Element getNext() {
         return this.next;
     }
-
+    /**
+     * Set the next element
+     * @param next - the next element of the lane
+     */
     public void setNext(Element next) {
         this.next = next;
     }
-
+    /**
+     * @return - the value of the element
+     */
     public E getValue() {
         return value;
     }
-
+    /**
+     * Set the new value of the element
+     * @param value - the new value to set
+     */
     public void setValue(E value) {
         this.value = value;
     }
--- UV-Langages-de-programmation/src/tp1/Lane.java
+++ UV-Langages-de-programmation/src/tp1/Lane.java
@@ -1,24 +1,27 @@
 package tp1;
-
+/**
+ * Interface of a model for a Lane (succession of element)
+ * @param <E> the type of element in the list
+ */
 public interface Lane<E> {
-	
-	/**
-	 * Add an element to the lane 
-	 */
+
+    /**
+     * Add an element to the lane
+     */
     public void pushElem(E e);
-    
+
     /**
      * Remove an element of the lane
      * @return The element removed
      */
     public Object popElem();
-    
+
     /**
      * Check if the lane is empty
      * @return true if the lane is empty
      */
     public boolean isEmpty();
-    
+
     /**
      * @return the lane's length
      */
