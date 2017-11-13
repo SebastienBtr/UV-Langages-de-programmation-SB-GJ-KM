3cbfa9644e86d12947f7b78621fdd961b196ae68
GaetanJ <gaetan.jacquet@imt-atlantique.net>
2017-11-13 19:13:21 +0100

    We've completed list

--- UV-Langages-de-programmation/src/tp/v2/IterateurListe.java
+++ UV-Langages-de-programmation/src/tp/v2/IterateurListe.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 
 public class IterateurListe<E> implements Iterator<E> {
 
+
+
 	@Override
 	public boolean hasNext() {
 		// TODO Auto-generated method stub
--- UV-Langages-de-programmation/src/tp/v2/Liste.java
+++ UV-Langages-de-programmation/src/tp/v2/Liste.java
@@ -29,26 +29,94 @@ public interface Liste<E> extends Iterable<E> {
 	 * Services
 	 */
 	default Iterator<E> iterator() {
-		// TODO
-		return null; // Compléter puis utiliser IterateurListe.
+
+		return new IterateurListe<>(); // Compléter puis utiliser IterateurListe.
 	}
+
 	default Liste<E> miroir(){
-		// TODO
+
 		return null;
 	}
+
 	/*
 	 * Fabriques (statiques)
 	 */
-	
 	public static <E> Liste<E> vide() {
 		return new Liste<E>() {
-			// TODO Définir les méthodes utiles.			
+
+			public boolean casVide() {
+				return true;
+			}
+
+			public Iterator<E> iterator() {
+
+				return null;
+			}
+
+			public Liste<E> miroir(){
+
+				return this;
+			}
+
 		};
 	}
 	
 	public static <E> Liste<E> cons(E t, Liste<E> r) {
 		return new Liste<E>() {
-			// TODO Définir les méthodes utiles.			
+			/*
+			 * Accesseurs
+			 */
+			public E tete() {
+				return t;
+			}
+			public Liste<E> reste() { return r; }
+			public boolean casCons() {
+				return true;
+			}
+			public int taille(){
+				return 0;
+			}
+			public boolean estVide(){
+				return this.taille() == 0;
+			}
+
+
+			public Iterator<E> iterator() {
+				return new IterateurListe<E>() {
+					Liste<E> list = cons(t, r);
+
+					public boolean hasNext() {
+						return !list.estVide();
+					}
+
+					public E next() {
+						if (!hasNext()) {
+							throw new UnsupportedOperationException();
+						}
+
+						Liste<E> tmp = list.reste();
+						this.list = cons(tmp.tete(), tmp.reste());
+						return list.tete();
+					}
+				};
+			}
+
+			public Liste<E> miroir(){
+
+				Liste<E> inverted ;
+				Iterator<E> itr = this.iterator();
+				E next = this.tete();
+
+
+				while (itr.hasNext()) {
+					next = itr.next();
+				}
+
+				inverted = cons(next ,this.reste().miroir());
+
+				return inverted;
+			}
+
 		};
 	}
 	
